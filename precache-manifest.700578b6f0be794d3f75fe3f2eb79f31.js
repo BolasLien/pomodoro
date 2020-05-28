ead637840a76299359b9f98c56eba0de210a7859
BolasLien <ltzuen@gmail.com>
2020-05-28 21:56:36 +0800

    pwa build

---
+++ precache-manifest.700578b6f0be794d3f75fe3f2eb79f31.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "alarms/alarm1.mp3"
+  },
+  {
+    "revision": "aeeeb5ba4945961137d05fe48f5989c4",
+    "url": "alarms/alarm2.wav"
+  },
+  {
+    "revision": "6707e03b875adb4b656a",
+    "url": "css/app.45f14bd4.css"
+  },
+  {
+    "revision": "807fb5d10f3d9dfb13ad",
+    "url": "css/chunk-vendors.084c0951.css"
+  },
+  {
+    "revision": "2cbaa47c24a4ce03b33bdacb8dea3bfb",
+    "url": "index.html"
+  },
+  {
+    "revision": "6707e03b875adb4b656a",
+    "url": "js/app.e2b69ac8.js"
+  },
+  {
+    "revision": "807fb5d10f3d9dfb13ad",
+    "url": "js/chunk-vendors.1d2385d8.js"
+  },
+  {
+    "revision": "b6d1764145987c4daaf0",
+    "url": "js/list.6fe43a11.js"
+  },
+  {
+    "revision": "9a7eabbddd34c0d65387",
+    "url": "js/notfound.9bdb9b32.js"
+  },
+  {
+    "revision": "519c4df764bd30e11163",
+    "url": "js/settings.ebf942f2.js"
+  },
+  {
+    "revision": "3dc210960fe7e4127773465ac5d8a550",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
