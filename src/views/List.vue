f431d677704dcabd1262b4e6bd9b094353771796
BolasLien <ltzuen@gmai.com>
2020-06-01 16:33:05 +0800

    切版面

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,34 +1,52 @@
 <template>
-  <div id="list">
-    <b-form-input v-model="newtodo"></b-form-input>
-    <b-btn variant="success" @click="addTodo">新增</b-btn>
-    <b-table-simple>
-      <draggable v-model="todos" tag="tbody" v-bind="dragOption">
-        <b-tr v-if="todos.length == 0">
-          <b-td>沒有資料</b-td>
-        </b-tr>
-        <b-tr v-else v-for="(todo, index) in todos" :key="index">
-          <b-td>
-            <b-form-input v-model="todo.model" v-if="todo.edit"></b-form-input>
-            <b-btn variant="link" class="text-danger"  v-if="todo.edit" @click="cancelTodo(index)">
-              <font-awesome-icon :icon="['fas','window-close']"></font-awesome-icon>
-            </b-btn>
-            <b-btn variant="link" class="text-success" v-if="todo.edit" @click="saveTodo(index)">
-              <font-awesome-icon :icon="['fas','save']"></font-awesome-icon>
-            </b-btn>
-            <span v-else>{{ todo.name }}</span>
+  <div id="list" class="row flex-column">
+    <div class="col mb-3">
+      <div class="row">
+        <div class="col-8">
+          <b-form-input v-model="newtodo" placeholder="新增待辦事項..."></b-form-input>
+        </div>
+        <div class="col-2">
+          <b-btn variant="success" @click="addTodo">
+            <font-awesome-icon :icon="['fas','plus']"></font-awesome-icon>
+          </b-btn>
+        </div>
+        <div class="col-2">
+          <b-btn :variant="editColor" @click="listEdit">
+            <font-awesome-icon :icon="['fas','pen']"></font-awesome-icon>
+          </b-btn>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <b-table-simple>
+        <draggable :sort="isEdit" v-model="todos" tag="tbody" v-bind="dragOption">
+          <b-tr v-if="todos.length == 0">
+            <b-td>沒有資料</b-td>
+          </b-tr>
+          <b-tr v-else v-for="(todo, index) in todos" :key="index">
+            <b-td>
+              <b-form-input v-model="todo.model" v-if="todo.edit"></b-form-input>
+              <b-btn variant="link" class="text-danger" v-if="todo.edit" @click="cancelTodo(index)">
+                <font-awesome-icon :icon="['fas','window-close']" size="2x"></font-awesome-icon>
+              </b-btn>
+              <b-btn variant="link" class="text-success" v-if="todo.edit" @click="saveTodo(index)">
+                <font-awesome-icon :icon="['fas','save']" size="2x"></font-awesome-icon>
+              </b-btn>
+              <span v-else>{{ todo.name }}</span>
             </b-td>
-          <b-td>
-            <b-btn variant="link" class="text-primary" @click="editTodo(index)">
-              <font-awesome-icon :icon="['fas','pen']"></font-awesome-icon>
-            </b-btn>
-            <b-btn variant="link" class="text-danger" @click="delTodo(index)">
-              <font-awesome-icon :icon="['fas','trash-alt']"></font-awesome-icon>
-            </b-btn>
-          </b-td>
-        </b-tr>
-      </draggable>
-    </b-table-simple>
+            <b-td>
+              <b-btn v-if="isEdit" variant="link" class="text-primary" @click="editTodo(index)">
+                <font-awesome-icon :icon="['fas','pen']"></font-awesome-icon>
+              </b-btn>
+              <b-btn v-if="isEdit" variant="link" class="text-danger" @click="delTodo(index)">
+                <font-awesome-icon :icon="['fas','trash-alt']"></font-awesome-icon>
+              </b-btn>
+            </b-td>
+          </b-tr>
+        </draggable>
+      </b-table-simple>
+    </div>
   </div>
 </template>
 
@@ -37,6 +55,8 @@ export default {
   data () {
     return {
       newtodo: '',
+      isEdit: false,
+      editColor: 'primary',
       dragOption: {
         animation: 200
       }
@@ -57,6 +77,10 @@ export default {
     },
     saveTodo (index) {
       this.$store.commit('saveTodo', index)
+    },
+    listEdit () {
+      this.isEdit = !this.isEdit
+      this.editColor = this.isEdit ? 'danger' : 'primary'
     }
   },
   computed: {
