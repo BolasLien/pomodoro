6b4990cb65bfec7995255aadacd2155b7a1dd552
BolasLien <ltzuen@gmail.com>
2020-07-01 16:23:04 +0800

    引用Vue2-timepick

--- src/main.js
+++ src/main.js
@@ -14,15 +14,18 @@ import { faCheck, faPen, faTrashAlt, faSave, faWindowClose, faPlay, faPause, faS
 
 import draggable from 'vuedraggable'
 import VueEllipseProgress from 'vue-ellipse-progress'
+import VueTimepicker from 'vue2-timepicker'
 
 import 'noto-sans-tc/noto_sans_tc_regular/css.css'
 import './style/style.styl'
+import 'vue2-timepicker/dist/VueTimepicker.css'
 
 library.add(faCheck, faPen, faTrashAlt, faSave, faWindowClose, faPlay, faPause, faStepForward, faPlusCircle, faCog, faClock, faPlus, faTasks)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.component('draggable', draggable)
 Vue.use(VueEllipseProgress, 'vep')
+Vue.component('VueTimepicker', VueTimepicker)
 
 Vue.use(VueGtag, {
   config: { id: 'UA-168227875-1' }
